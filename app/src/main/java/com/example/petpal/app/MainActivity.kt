ea2f9ac346f4ee815a7c17ec951e167a57cae4b2
AdrianTruong <165998998+AdrianTruong@users.noreply.github.com>
2025-07-24 18:46:45 +0700

    Change the directory according to the MVVM

--- app/src/main/java/com/example/petpal/app/MainActivity.kt
+++ app/src/main/java/com/example/petpal/app/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.petpal.ui.theme.PetPalTheme
+import com.example.petpal.app.theme.PetPalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
